dc20da2c41561e3b7ef7f12f541a6a6c5f4f6a2d
Nguyen Ngoc Duc <nguyenngocduc.tosa@gmail.com>
2022-08-14 23:51:21 +0700

    first commit

---
+++ lib/component/mycolor.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color kOnboardColor1 = const Color(0xffDAD3C8);
+Color kOnboardColor2 = const Color(0xffFFE5DE);
+Color kOnboardColor3 = const Color(0xffDCF6E6);
